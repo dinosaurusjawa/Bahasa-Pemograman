eb0b4b776a8ff992132e89ab052205a650dd2598
root <root@LAPTOP-LTKGPQOB>
2024-03-30 14:11:42 +0700

    perubahan

---
+++ pert1/samplearray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a [5] = {1,2,3,4,5};
+    cout << a[0];
+    cout << a[1];
+    cout << a[2];
+    cout << a[3];
+    cout << a[4];
+    return 0;
+}
