04492e125d66ec0d1ad13d3c5f4852c7ec02c800
root <root@LAPTOP-LTKGPQOB>
2024-04-06 09:09:29 +0700

    perubahan

---
+++ pert3/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void tambah() {
+    int a = 5;
+    int b = 7;
+    int c = a + b;
+    cout << c << endl;
+    
+}
+int main() {
+    tambah();
+    return 0;
+
+}
