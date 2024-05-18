6ebea0e266976c362e255f24a67c32361c87bbd1
root <root@LAPTOP-LTKGPQOB>
2024-05-18 09:29:39 +0700

    contoh-uts

--- soal-uts/Nama-Nim.md
+++ soal-uts/Nama-Nim.md
@@ -1,17 +1,20 @@
 # SOAL 1
 Jelaskan esuai dengan pemahaman anda apa itu Bahasa pemograman?
 # JAWABAN
-## JAWABAN ANDA
+## Bahasa pemograman adalah instruksi standar untuk memerintah komputer.
 
 # SOAL 2
-Jelaskan berdasarkan pemahaman anda apa saja paradigman dalam Bahasa pemograman?
+Jelaskan berdasarkan pemahaman anda apa saja paradigma dalam Bahasa pemograman?
 # JAWABAN
-## JAWABAN ANDA
+## Paradigma pemrograman merupakan gaya, klasifikasi, dan pendekatan dalam penulisan program untuk memecahkan masalah dengan menggunakan bahasa pemrograman yang digunakan.
 
 # SOAL 3
 jelaskan type data yang anda ketahui dan berikan penjelasan terkait type data tersebut
 # JAWABAN
-## JAWABAN ANDA
+## Type data adalah jenis data yang mampu ditangani oleh suatu bahasa pemograman pada komputer dan Tiap-tiap bahasa pemrograman memiliki tipe data yang memungkinkan:
+Deklarasi terhadap variabel tipe data tersebut
+Menyediakan kumpulan operasi yang mungkin terhadap variabel bertipe data tersebut
+Jenis obyek data yang mungkin Contoh tipe data di C++ Java Pascal .NET
 
 # SOAL 4
 Lengkapi codingan berikut:
@@ -26,10 +29,10 @@ int main(){
 ```	
 
 # JAWABAN
-## JAWABAN ANDA (Nama file cpp)
+## JAWABAN ANDA (essay4.cpp)
 
 ## STUDI KASUS 
 ## Verdasarkan soal essay no 4
 buatlah diagram alur/flow yang sesuai an source yang anda lengkapi
 # JAWABAN
-## JAWABAN ANDA (Nama file puml)
+## JAWABAN ANDA (studikasus.puml)
--- soal-uts/essay4.cpp
+++ soal-uts/essay4.cpp
@@ -1,6 +1,80 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-int main(){
+// Deklarasi fungsi
+void tambah();
+void kurang();
+void bagi();
+void kali();
+
+int main() {
+    int pilihan;
+    do {
+        printf("\nKalkulator Sederhana\n");
+        printf("1. Tambah\n");
+        printf("2. Kurang\n");
+        printf("3. Bagi\n");
+        printf("4. Kali\n");
+        printf("5. Keluar\n");
+        printf("Pilih operasi (1-5): ");
+        scanf("%d", &pilihan);
+
+        switch (pilihan) {
+            case 1:
+                tambah();
+                break;
+            case 2:
+                kurang();
+                break;
+            case 3:
+                bagi();
+                break;
+            case 4:
+                kali();
+                break;
+            case 5:
+                printf("Keluar dari program.\n");
+                break;
+            default:
+                printf("Pilihan tidak valid, coba lagi.\n");
+                break;
+        }
+    } while (pilihan != 5);
+
     return 0;
+}
+
+// Definisi fungsi tambah
+void tambah() {
+    int a, b;
+    printf("Masukkan dua angka untuk ditambah: ");
+    scanf("%d %d", &a, &b);
+    printf("Hasil: %d\n", a + b);
+}
+
+// Definisi fungsi kurang
+void kurang() {
+    int a, b;
+    printf("Masukkan dua angka untuk dikurang: ");
+    scanf("%d %d", &a, &b);
+    printf("Hasil: %d\n", a - b);
+}
+
+// Definisi fungsi bagi
+void bagi() {
+    float a, b;
+    printf("Masukkan dua angka untuk dibagi: ");
+    scanf("%f %f", &a, &b);
+    if (b != 0) {
+        printf("Hasil: %.2f\n", a / b);
+    } else {
+        printf("Kesalahan: Pembagian dengan nol tidak diperbolehkan.\n");
+    }
+}
+
+// Definisi fungsi kali
+void kali() {
+    int a, b;
+    printf("Masukkan dua angka untuk dikali: ");
+    scanf("%d %d", &a, &b);
+    printf("Hasil: %d\n", a * b);
 }
